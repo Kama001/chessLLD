8e1fcd1702106196ebdc4eb8a361d1f72021d89b
Kama001 <saic3076@gmail.com>
2024-05-06 19:31:25 +0530

    fix pointer issue

--- piece.hpp
+++ piece.hpp
@@ -4,18 +4,13 @@ class Piece {
     public: 
         bool white = false;
         bool killed = false;
-        // Piece(bool white) {
-        //     this->white = white;
-        // }
         bool isWhite() {
-            // return this->white;
             return white;
         }
         void setWhite(bool white) {
             this->white = white;
         }
         bool isKilled() {
-            // return this->killed;
             return killed;
         }
         void setKilled(bool killed) {
@@ -29,9 +24,9 @@ class Pawn: public Piece {
         return true;
     }
     public:
-        void show() {
-            cout << this->white << " " << this->killed; 
-        }
+        // void show() {
+        //     cout << this->white << " " << this->killed; 
+        // }
         Pawn(bool white, bool killed) {
             setWhite(white);
             setKilled(killed);
